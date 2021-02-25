5ecc4751309a456ba19752ba492d68908a0967f8
kali151 <cloud17@gmail.com>
2021-02-25 15:28:02 +0100

    Basic input improvements

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,15 +4,42 @@ import classes from './Input.css'
 const input = (props) => {
     let inputEl = null;
 
-    switch (props.inputtype) {
+    switch (props.elementType) {
         case ('input'):
-            inputEl = <input className={classes.InputEl} {...props} />;
+            inputEl = <input
+                className={classes.InputEl}
+                {...props.elementConfig}
+                value={props.value} 
+                onChange={props.changed}/>;
             break;
         case ('textarea'):
-            inputEl = <textarea className={classes.InputEl} {...props} />;
+            inputEl = <input
+                className={classes.InputEl}
+                {...props.elementConfig}
+                value={props.value} 
+                onChange={props.changed}/>;
             break;
+        case ('select'):
+            inputEl = (
+            <select
+                className={classes.InputEl}
+                value={props.value} 
+                onChange={props.changed}>
+                {props.elementConfig.options.map(option => (
+                    <option key={option.value}
+                        value={option.value}>
+                        {option.displayValue}
+                    </option>
+                ))}
+            </select>
+            );
+            break;            
         default:
-            inputEl = <input className={classes.InputEl} {...props} />;
+            inputEl = <input
+                className={classes.InputEl}
+                {...props.elementConfig}
+                value={props.value} 
+                onChange={props.changed}/>;
             break;
     }
 
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,31 +8,98 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'your name'
+                },
+                value: 'Coco',
+                valid: false,
+                validation: {
+                    required: true
+                }
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'your street'
+                },
+                value: '',
+                valid: false,
+                validation: {
+                    required: true
+                }
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'your zip code'
+                },
+                value: '',
+                valid: false,
+                validation: {
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5
+                }
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'your country'
+                },
+                value: '',
+                valid: false,
+                validation: {
+                    required: true
+                }
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'your mail'
+                },
+                value: '',
+                valid: false,
+                validation: {
+                    required: true
+                }
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'fastest', diplayValue: 'Fast'},
+                        {value: 'cheapest', diplayValue: 'Cheap'}
+                    ]
+                },
+                value: 'fastest'
+            }
         },
         loading: false
     }
 
+    
     orderHandler = ( event ) => {
         event.preventDefault();
         this.setState( { loading: true } );
+        const formData = {};
+
+        for (let formEl in this.state.orderForm) {
+            formData[formEl] = this.state.orderForm[formEl].value;
+        }
+      //  console.log("form data: ", formData);
+
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            customer: {
-                name: 'Coco chanell',
-                address: {
-                    street: 'test',
-                    zipCode: '225566',
-                    country: 'Poland'
-                },
-                email: 'coco@cocosha.com'
-            },
-            deliveryMethod: 'fastest'
+            orderData: formData
         }
         axios.post( '/orders.json', order )
             .then( response => {
@@ -44,14 +111,68 @@ class ContactData extends Component {
             } );
     }
 
+    validateForm(value, rules) {
+        let isValid= true;
+
+        if(rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if(rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+        if(rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+         
+        return isValid;
+    }
+
+    inputChangedHandler = (event, inputId) => {
+        //console.log(event.target.value);
+        const updatedForm = {
+            ...this.state.orderForm
+        };
+        //clone deeply
+        const updatedElement = {
+            ...updatedForm[inputId]
+        };
+        updatedElement.value = event.target.value;
+        // validation
+        updatedElement.valid = this.validateForm(updatedElement.value, updatedElement.validation);
+        updatedForm[inputId] = updatedElement;
+
+        console.log("updatedForm: ", updatedForm.zipCode.valid);
+        this.setState({orderForm: updatedForm});
+    }
+
     render () {
+        // obj to array
+        const formElements = [];
+        for (let key in this.state.orderForm) {
+            formElements.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+
         let form = (
-            <form>
-                <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
-                <Input inputtype="input" type="email" name="email" placeholder="Your Mail" />
+            <form onSubmit={this.orderHandler}>
+                {/* <Input elementType="..." elementConfig="..." value="..." /> */}
+                {formElements.map(el => (
+                    <Input 
+                        key={el.id}
+                        elementType={el.config.elementType}
+                        elementConfig={el.config.elementConfig}
+                        value={el.config.value}
+                        changed={(event) => this.inputChangedHandler(event, el.id)}/>
+                )
+
+                )}
+                {/* <Input inputtype="input" type="email" name="email" placeholder="Your Mail" />
                 <Input inputtype="input" type="text" name="street" placeholder="Street" />
-                <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
-                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+                <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" /> */}
+                <Button btnType="Success" >ORDER</Button>
             </form>
         );
         if ( this.state.loading ) {
