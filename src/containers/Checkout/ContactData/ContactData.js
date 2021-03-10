b8f5744ab774088b2dd1fef44ea8ea51abdec206
kali151 <cloud17@gmail.com>
2021-03-10 15:38:58 +0100

    Display orders only done by proper user

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -111,7 +111,8 @@ class ContactData extends Component {
         const order = {
             ingredients: this.props.ings,
             price: this.props.prc,
-            orderData: formData
+            orderData: formData,
+            userId: this.props.userId
         }
 
         this.props.onOrder(order, this.props.token)
@@ -211,7 +212,8 @@ const mapStateToProps = state => {
         ings: state.burgerBuilder.ingredients,
         prc: state.burgerBuilder.totalPrice,
         loading: state.order.loading,
-        token: state.auth.token
+        token: state.auth.token,
+        userId: state.auth.userId
     }
 };
 
